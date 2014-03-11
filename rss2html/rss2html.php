a05d1e5dd9e1a679e9fac5653ffd49fadd5b1fa6
Micah Redding <micahtredding@gmail.com>
2014-03-10 23:38:41 -0500

    Styling feed items

--- rss2html/rss2html.php
+++ rss2html/rss2html.php
@@ -467,7 +467,7 @@ if ($useFopenURL == 1) {
   ini_set("user_agent", "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.1;) Gecko/2008070208 Firefox/3.0.1");
 }
 
-$FeedMaxItems = 10000;
+$FeedMaxItems = 100;
 $NoFutureItems = FALSE;
 
 @include("FeedForAll_rss2html_pro.php");
